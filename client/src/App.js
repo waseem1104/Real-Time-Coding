795a6c20c775085ba17bbcacc43574aaa134948c
waseem1104 <waseem11@hotmail.fr>
2022-11-18 19:33:45 +0100

    Add Menu,Home and Login components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
+import {Routes,Route} from "react-router-dom";
+import Login from "./component/Login";
+import Home from './component/Home';
 function App() {
 
     return (
         <>
-            Welcome !
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/login" element={<Login/>}/>
+            </Routes>
         </>
     )
 
 }
-
 export default App;
---
+++ client/src/component/Menu.js
@@ -0,0 +1,25 @@
+import React,{Fragment} from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import {Link, useNavigate} from "react-router-dom";
+
+export default function Menu(){
+    return(
+        <Fragment>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Link to={'/login'} className={"nav-link"}>Connexion</Link>
+                            <Link to={'/login'} className={"nav-link"}>Inscription</Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+    </Fragment>
+    )
+}
