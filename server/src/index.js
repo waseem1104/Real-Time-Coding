f863f2b46db9388b0e8d4832a21c53e846cf1056
waseem1104 <waseem11@hotmail.fr>
2022-11-23 19:43:22 +0100

    Server - create routes admin and init socket room

--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,7 @@ const jwt = require("jsonwebtoken");
 
 
 const SecurityRouter = require("./routes/Security");
+const AdminRouter = require("./routes/Admin");
 
 const corsOption = {
     origin: ['http://localhost:3000'],
@@ -25,27 +26,29 @@ const io = new Server(
 
 app.use(express.json());
 app.use(cors(corsOption));
+
 app.use(SecurityRouter);
+app.use("/admin", AdminRouter);
 
 app.get("/", (req, res, next) => {
     res.send("Hello world!");
 });
 
-io.use(async(socket, next) => {
-  if (socket.handshake.auth && socket.handshake.auth.token){
-    const token = socket.handshake.auth.token;
-    const payload = await jwt.verify(token, process.env.JWT_SECRET);
-    socket.user_id = payload.id;
-    socket.is_admin = payload.isAdmin;
-    socket.email = payload.email;
-    next();
+// io.use(async(socket, next) => {
+//   if (socket.handshake.auth && socket.handshake.auth.token){
+//     const token = socket.handshake.auth.token;
+//     const payload = await jwt.verify(token, process.env.JWT_SECRET);
+//     socket.user_id = payload.id;
+//     socket.is_admin = payload.isAdmin;
+//     socket.email = payload.email;
+//     next();
   
-  }
-  else {
-    next(new Error('Authentication error'));
-  }    
-})
-.on('connection', function(socket) {
+//   }
+//   else {
+//     next(new Error('Authentication error'));
+//   }    
+// })
+io.on('connection', function(socket) {
 
     // List users connection
     let users = [];
@@ -54,8 +57,12 @@ io.use(async(socket, next) => {
         email : socket.email
       })
     }
-    
+
     socket.emit("users",users);
+
+    socket.on('createRoom',(name)=>{
+      socket.broadcast.emit("getRoom",name);
+    });
 });
 
 http.listen(process.env.PORT, () => {
---
+++ server/src/routes/Admin.js
@@ -0,0 +1,31 @@
+const { Router } = require("express");
+const {Room} = require("../models/postgres");
+const {ValidationError, QueryTypes} = require("sequelize");
+
+
+const router = new Router();
+
+router.post("/room/new", async (req, res) => {
+    const name = req.body.name.trim();
+
+    try {
+        const result = await Room.create({
+            name: name,
+            limit: 1,
+            status:1
+        });
+
+        res.status(201).json(result);
+    } catch (error) {
+
+        if (error instanceof ValidationError) {
+            res.status(422).json(formatError(error));
+        } else {
+            res.sendStatus(500);
+            console.error(error);
+        }
+    }
+});
+
+
+module.exports = router;
