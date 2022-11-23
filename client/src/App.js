9dc7b2a371af2f07c011e6ba71f175e5514c4d89
waseem1104 <waseem11@hotmail.fr>
2022-11-23 19:38:24 +0100

    Client - Add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import {Routes,Route} from "react-router-dom";
 import Login from "./component/Login";
 import Home from './component/Home';
+import Admin from "./component/Admin";
+import CreateRoom from "./component/CreateRoom";
+import ListRooms from "./component/ListRooms";
 function App() {
 
     return (
@@ -8,6 +11,9 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/login" element={<Login/>}/>
+                <Route path="/admin" element={<Admin/>}/>
+                <Route path="/admin/room/new" element={<CreateRoom/>}/>
+                <Route path="/rooms" element={<ListRooms/>}/>
             </Routes>
         </>
     )
