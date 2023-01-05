98924e900e24f69e4bcfb5c46bec768001cc8a20
waseem1104 <waseem11@hotmail.fr>
2023-01-05 16:16:12 +0100

    Chatbot

--- client/src/component/front/chatbot/Chatbot.js
+++ client/src/component/front/chatbot/Chatbot.js
@@ -15,16 +15,61 @@ export default function Chatbot(){
     const socket = useSocket();
 
     const [messages,setMessages] = useState([]);
+    const [message,setMessage] = useState("");
+    const [step,setStep] = useState(0);
+
+
+    useEffect( () => {
+        socket.emit('chatbot',({client: 1, step: 0, message : null}));
+    },[])
+    
+    const handleSubmit = useCallback( () => {
+
+        let sliceMessages = messages.slice();
+        sliceMessages.push({content:message,is:"client"})
+        setMessages(sliceMessages);
+
+        if (step == 1){
+            socket.emit('chatbot',({client: 1, step: 1, message : message}));
+        }
+
+        if (step == 3){
+            socket.emit('chatbot',({client: 1, step: 3, message : message}));
+        }
+
+        if (step == 4){
+            socket.emit('chatbot',({client: 1, step: 4, message : message}));
+        }
+
+        if (step == 41){
+            socket.emit('chatbot',({client: 1, step: 41, message : message}));
+        }
+
+
+        if (step == 42){
+            socket.emit('chatbot',({client: 1, step: 42, message : message}));
+        }
+
+        if (step == 43){
+            socket.emit('chatbot',({client: 1, step: 43, message : message}));
+        }
+
+        if (step == -1){
+            socket.emit('chatbot',({client: 1, step: 0, message : null}));
+        }
+
+    },[step,message,messages])
 
     useEffect( () => {
 
-        socket.emit('starting chatbot',({client: 1}));
-
-        socket.on('starting chatbot',({content}) => {
-            console.log(content);
+        socket.on('chatbot',({content,is,step}) => {
+            let sliceMessages = messages.slice();
+            sliceMessages.push({content,is})
+            setMessages(sliceMessages)
+            setStep(step)
         })
+    },[messages])
 
-    },[])
     return(
         <Fragment>
             <Menu/>
@@ -34,15 +79,34 @@ export default function Chatbot(){
                 <Row className="mt-5">
                 <h2 className="fs-5 mb-3">Chatbot</h2>
                     <Col>
-                    <Card style={{height: '30rem'}}>
-                            <Card.Body>
+                    <Card id="chatbot" style={{height: '30rem'}}>
+                            <Card.Body id="chatbot-body">
+                            {
+                                messages.map( (message,i) =>{
+                                    return(
+                                        <div className="message mb-2" key={i}>
+                                        { message.is == "chatbot" ? 
+                                            <div className="content-chatbot px-2 d-flex justify-content-start">
+                                                <span>{message.content}</span>
+                                            </div>
+                                            : 
+                                            <div className="content-client px-2 d-flex justify-content-end">
+                                                <span>{message.content}</span>
+                                            </div>
+                                        }
+                                        </div>
+                                    );
+                                })
+                            }
                             </Card.Body>
                             <Card.Footer>
                             <InputGroup>
                                 <Form.Control
                                 placeholder="Message..."
+                                value={message}
+                                onChange={ (e) => setMessage(e.target.value)}
                                 />
-                                <Button variant="dark">Envoyer</Button>
+                                <Button variant="dark" onClick={ handleSubmit }>Envoyer</Button>
                             </InputGroup>
                             </Card.Footer>
                         </Card>
--- server/src/index.js
+++ server/src/index.js
@@ -36,6 +36,32 @@ app.get("/", (req, res, next) => {
     res.send("Hello world!");
 });
 
+const chatbotTyping = (to,content,is,step) => {
+  io.sockets.to(to).emit("chatbot",{
+    content: content,
+    is: is,
+    step: step
+  })
+}
+
+const dateFormat = (dateToFormat) => {
+  let dateSplited = dateToFormat.split('/');
+  let newDate = dateSplited[1] + "/" + dateSplited[0] + "/" +dateSplited[2]; 
+
+  return newDate;
+}
+
+const ValidDate = (day) => {
+  var bitsDays = day.split('/');
+  var yDays = bitsDays[2], 
+  mDays  = bitsDays[1],
+  dDays = bitsDays[0];
+  var daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
+  if (yDays % 400 === 0 || (yDays % 100 !== 0 && yDays % 4 === 0)) {
+    daysInMonth[1] = 29;
+  }
+  return dDays <= daysInMonth[--mDays];
+}
 
 const sessions = new Map();
 
@@ -82,14 +108,103 @@ io.on('connection', function(socket) {
 
     socket.on('room created',(room)=>{
       io.sockets.emit("get room",room);
-    });
+    }); 
 
 
-    socket.on('starting chatbot', ({client}) => {
+    socket.on('chatbot', ({client,step,message}) => {
 
-      socket.to(client).emit("starting chatbot",{
-        content: "hello"
-      })
+      if (step == 0){
+        chatbotTyping(client,`Bonjour ${socket.email} ! 1/ Entretien 2/ Informations 3/ Contact 4/ Fin.`,'chatbot',1);
+      }
+
+      if (step == 1){
+        switch(message){
+          case '1':
+            chatbotTyping(client,"Année ?",'chatbot',4);
+            break;
+          case '3':
+            chatbotTyping(client,`Qu'avez-vous besoin ? 1/ Email 2/ Numéro de téléphone.`,'chatbot',3);
+            break;
+          case '4':
+            chatbotTyping(client,'Merci et à très bientôt !','chatbot',-1);
+            break;
+          default:
+            chatbotTyping(client,`Bonjour ${socket.email} ! 1/ Entretien 2/ Informations 3/ Contact 4/ Fin.`,'chatbot',1);
+        }
+      }
+
+      if (step == 3){
+        switch(message){
+          case '1':
+            chatbotTyping(client,'test@test.com.','chatbot',31);
+            chatbotTyping(client,'Merci et à très bientôt !','chatbot',-1);
+            break;
+
+          case '2':
+            chatbotTyping(client,'0102030405','chatbot',31);
+            chatbotTyping(client,'Merci et à très bientôt !','chatbot',-1);
+            break;
+
+          default:
+            chatbotTyping(client,`Qu'avez-vous besoin ? 1/ Email 2/ Numéro de téléphone.`,'chatbot',3);
+        }
+      }
+
+      if (step == 4){
+        if (isNaN(message)){
+          chatbotTyping(client,`Erreur au niveau de l'année !`,'chatbot',4);
+          chatbotTyping(client,"Année ?",'chatbot',4);
+        }else{
+          chatbotTyping(client,"Date de dernier entretien du véhicule ? (DD/MM/YYYY)",'chatbot',41);
+        }
+      }
+
+      if (step == 41){
+        if (!ValidDate(message)){
+          chatbotTyping(client,"Erreur",'chatbot',41);
+          chatbotTyping(client,"Date de dernier entretien du véhicule ? (DD/MM/YYYY)",'chatbot',41);
+        }
+        else{
+          // socket.lastyear = message;
+          let newDate = new Date(dateFormat(message));
+          if (newDate.setMonth(newDate.getMonth()+12) < new Date() ){
+            chatbotTyping(client,"Nombre de KM parcourus depuis le dernier entretien ?",'chatbot',42);
+          }else{
+
+
+            // DISPO
+
+          }
+        }
+      }
+
+      if (step == 42){
+        if (isNaN(message)){
+          chatbotTyping(client,"Erreur ! Nombre de KM parcourus depuis le dernier entretien ?",'chatbot',42);
+        }else{
+          if (message >= 10000 ){
+
+          }else{
+            chatbotTyping(client,"Souhaitez-vous réviser votre véhicule ? 1/ Oui 2/ Non.",'chatbot',43);
+          }
+        }
+      }
+
+      if (step == 43){
+        switch(message){
+          case '1':
+            break;
+          case "2":
+            chatbotTyping(client,'Merci et à très bientôt !','chatbot',-1);
+            chatbotTyping(client,`Bonjour ${socket.email} ! 1/ Entretien 2/ Informations 3/ Contact 4/ Fin.`,'chatbot',1);
+            break;
+          default:
+            chatbotTyping(client,"Souhaitez-vous réviser votre véhicule ? 1/ Oui 2/ Non.",'chatbot',43);
+        }
+      }
+
+
+      
     })
 
     socket.on('room updated',(room)=>{
