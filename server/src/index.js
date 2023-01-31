7dfaae05aaecb37ec057cc30764c7fb7d5a4012a
jehanebnj <jehanebnj@gmail.com>
2023-01-31 20:22:43 +0100

    comment see notification

--- server/src/index.js
+++ server/src/index.js
@@ -65,56 +65,56 @@ app.get('/', (req, res) => {
 
 // NOTIFICATION - SSE
 //---------------------------------
-app.get('/status', (request, response) => response.json({clients: clients.length}));
+// app.get('/status', (request, response) => response.json({clients: clients.length}));
 
-let clients = [];
-let facts = [];
+// let clients = [];
+// let facts = [];
 
-function eventsHandler(request, response, next) {
-    const headers = {
-        'Content-Type': 'text/event-stream',
-        'Connection': 'keep-alive',
-        'Cache-Control': 'no-cache'
-    };
-    response.writeHead(200, headers);
+// function eventsHandler(request, response, next) {
+//     const headers = {
+//         'Content-Type': 'text/event-stream',
+//         'Connection': 'keep-alive',
+//         'Cache-Control': 'no-cache'
+//     };
+//     response.writeHead(200, headers);
 
-    const data = `data: ${JSON.stringify(facts)}\n\n`;
+//     const data = `data: ${JSON.stringify(facts)}\n\n`;
 
-    response.write(data);
+//     response.write(data);
 
-    const clientId = Date.now();
+//     const clientId = Date.now();
 
-    const newClient = {
-        id: clientId,
-        response
-    };
+//     const newClient = {
+//         id: clientId,
+//         response
+//     };
 
-    clients.push(newClient);
+//     clients.push(newClient);
 
-    request.on('close', () => {
-        console.log(`${clientId} Connection closed`);
-        facts = [];
-        clients = clients.filter(client => client.id !== clientId);
-    });
-}
+//     request.on('close', () => {
+//         console.log(`${clientId} Connection closed`);
+//         facts = [];
+//         clients = clients.filter(client => client.id !== clientId);
+//     });
+// }
 
-app.get('/events', eventsHandler);
+// app.get('/events', eventsHandler);
 
-function sendEventsToAll(newFact) {
-    clients.forEach(client => client.response.write(`data: ${JSON.stringify(newFact)}\n\n`))
-}
+// function sendEventsToAll(newFact) {
+//     clients.forEach(client => client.response.write(`data: ${JSON.stringify(newFact)}\n\n`))
+// }
 
-async function addFact(request, res, next) {
-    const newFact = request.body;
-    facts.push(newFact);
-    setInterval(() => {
-        facts.pop();
-    }, 10000);
-    res.json(newFact)
-    return sendEventsToAll(newFact);
-}
+// async function addFact(request, res, next) {
+//     const newFact = request.body;
+//     facts.push(newFact);
+//     setInterval(() => {
+//         facts.pop();
+//     }, 10000);
+//     res.json(newFact)
+//     return sendEventsToAll(newFact);
+// }
 
-app.post('/fact', addFact);
+// app.post('/fact', addFact);
 
 const chatbotTyping = (
     to, content, is, step
